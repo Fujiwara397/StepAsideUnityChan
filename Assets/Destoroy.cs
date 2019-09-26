73ca3ee6b9adbcf0b0974e547333f02a2b7e511a
藤原靖浩 <fujiwara397@gmail.com>
2019-09-26 22:56:45 +0900

    レッスン６課題　再提出

--- Assets/Destoroy.cs
+++ Assets/Destoroy.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Destoroy : MonoBehaviour {
 
+	//メインカメラのオブジェクト
+	private GameObject MainCamera;
+	//アタッチされたオブジェクトとメインカメラの距離
+	private float difference;
+
 	// Use this for initialization
 	void Start () {
-		
+		//メインカメラのオブジェクトを取得
+		this. MainCamera= GameObject.Find ("Main Camera");
+
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
-	void OnBecameInvisible(){
-		Destroy (this.gameObject);
+	void Update () {//メインカメラの位置に越えたらオブジェクト消滅
+		if (MainCamera.transform.position.z > this.transform.position.z) {
+			Destroy (this.gameObject);
+
+		}
 }
 }
